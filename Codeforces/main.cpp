e9b628b768cf2c43d9722253cc2cee2f79a1c6eb
Vic Wen <vic20050418@gmail.com>
2023-06-15 05:57:46 +0800

    update

--- Codeforces/main.cpp
+++ Codeforces/main.cpp
@@ -2,9 +2,17 @@
 using namespace std;
 
 void solve() {
-  int m, n;
-  cin >> m >> n;
-  cout << m * n / 2;
+  int n, sum = 0;
+  string s;
+  cin >> n;
+  while (n--) {
+    cin >> s;
+    if (s[1] == '+')
+      sum++;
+    else
+      sum--;
+  }
+  cout << sum;
 }
 
 int main() {
