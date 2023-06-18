f1cdbb908fe4259d11b13915b7ff2b8eb1d8ea4e
Vic Wen <vic20050418@gmail.com>
2023-06-19 06:15:02 +0800

    update

--- CPE49/UVA10222-Decode the Mad man.cpp
+++ CPE49/UVA10222-Decode the Mad man.cpp
@@ -1,21 +1,24 @@
 #include <bits/stdc++.h>
 using namespace std;
-// 注意反斜線要反反斜線
-string table = "1234567890-=qwertyuiop[]\\asdfghjkl;'zxcvbnm,./";
+// 嚙窯嚙瞇嚙誕斜線嚙緯嚙誕反斜線
+string table = "`1234567890-=qwertyuiop[]\\asdfghjkl;'zxcvbnm,./";
 
 void solution() {
-	string s; //cout << s << '\n';
+  string s;  // cout << s << '\n';
 
-	while (getline(cin, s)) {
-		for (int i = 0; i < s.size(); i++) {
-			if (s[i] == ' ') cout << ' ';
-			else cout << table[table.find(tolower(s[i])) - 2];
-		}
-		cout << '\n';
-	}
+  while (getline(cin, s)) {
+    for (int i = 0; i < s.size(); i++) {
+      if (s[i] == ' ')
+        cout << ' ';
+      else
+        cout << table[table.find(tolower(s[i])) - 2];
+    }
+    cout << '\n';
+  }
 }
 
 int main() {
-	ios_base::sync_with_stdio(false); cin.tie(0);
-	solution();
+  ios_base::sync_with_stdio(false);
+  cin.tie(0);
+  solution();
 }
