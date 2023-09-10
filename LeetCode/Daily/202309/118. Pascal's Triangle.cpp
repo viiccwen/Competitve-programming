e529ce2275f2fb27e6d2ecb7e45ed2f018ae47fc
Vic Wen <vic20050418@gmail.com>
2023-09-10 14:05:24 +0800

    202309 Day 8

---
+++ LeetCode/Daily/202309/118. Pascal's Triangle.cpp
@@ -0,0 +1,14 @@
+class Solution {
+ public:
+  vector<vector<int>> generate(int numRows) {
+    vector<vector<int>> pas(numRows);
+    for (int i = 0; i < numRows; i++) {
+      pas[i].assign(i + 1, 1);
+      for (int j = 1; j < i; j++) {
+        pas[i][j] = pas[i - 1][j - 1] + pas[i - 1][j];
+      }
+    }
+
+    return pas;
+  }
+};
