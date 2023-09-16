24cb9b291b4d644188a03ec5623da72c26e51d6e
Vic Wen <vic20050418@gmail.com>
2023-09-16 21:40:58 +0800

    202309 Day 16

---
+++ LeetCode/Daily/202309/1631. Path With Minimum Effort.cpp
@@ -0,0 +1,36 @@
+class Solution {
+ public:
+  vector<int> DIR = {0, 1, 0, -1, 0};
+  int minimumEffortPath(vector<vector<int>>& h) {
+    int m = h.size(), n = h[0].size();
+    vector<vector<int>> dist(m, vector<int>(n, INT_MAX));
+    priority_queue<vector<int>, vector<vector<int>>, greater<vector<int>>>
+        minHeap;
+
+    minHeap.push({0, 0, 0});
+    dist[0][0] = 0;
+
+    while (!minHeap.empty()) {
+      vector<int> top = minHeap.top();
+      minHeap.pop();
+
+      int d = top[0], r = top[1], c = top[2];
+
+      if (r == m - 1 && c == n - 1) return d;
+
+      for (int i = 0; i < 4; i++) {
+        int nr = r + DIR[i], nc = c + DIR[i + 1];
+
+        if (nr >= 0 && nr < m && nc >= 0 && nc < n) {
+          int newDist = max(d, abs(h[nr][nc] - h[r][c]));
+
+          if (newDist < dist[nr][nc]) {
+            dist[nr][nc] = newDist;
+            minHeap.push({dist[nr][nc], nr, nc});
+          }
+        }
+      }
+    }
+    return 0;
+  }
+};
