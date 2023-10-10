ee69a9d7b505798c74f698820b44462ae52c44e2
Vic Wen <vic20050418@gmail.com>
2023-10-10 22:56:46 +0800

    update CPE49

--- CPE/CPE49/UVA490-Rotating Sentences.cpp
+++ CPE/CPE49/UVA490-Rotating Sentences.cpp
@@ -1,24 +1,49 @@
+
 #include <bits/stdc++.h>
 using namespace std;
 
-vector<string> v;
+void solve() {
+  vector<string> vec(101);
+  int idx = 0, maximum = 0;
+
+  while (getline(cin, vec[idx])) {
+    int len = vec[idx].size();
+    maximum = max(maximum, len);
+    idx++;
+  }
+
+  for (int i = 0; i < vec.size(); i++) {
+    for (int j = 0; j < vec[i].size(); j++) {
+      if (vec[i][j] == ' ') vec[i][j] = '\0';
+    }
+  }
+
+  for (int i = 0; i < maximum; i++) {
+    string cur = "";
+    for (int j = idx - 1; j >= 0; j--) {
+      if (vec[j].size() > i)
+        cur += vec[j][i];
+      else
+        cur += ' ';
+    }
+
+    int end = cur.size() - 1;
+
+    while (cur[end] == ' ') end--;
+
+    for (int i = 0; i <= end; i++) {
+      if (cur[i] == '\0')
+        cout << ' ';
+      else
+        cout << cur[i];
+    }
+    cout << '\n';
+  }
+}
 
 int main() {
-	ios_base::sync_with_stdio(false); cin.tie(0);
-	string temp;
-	int i = 0, max_len = 0;
-
-	while (getline(cin, temp)) {
-		v.push_back(temp);
-		max_len = max(max_len, int(v[i].size()));
-		i++;
-	}
-
-	for (int j = 0; j < max_len; j++) {
-		for (int k = i - 1; k >= 0; k--) {
-			if (j >= v[k].size() && k != 0) cout << " ";
-			else cout << v[k][j];
-		}
-		cout << '\n';
-	}
+  ios_base::sync_with_stdio(false);
+  cin.tie(0);
+  solve();
+  return 0;
 }
