2591523d88427d85fd55ef847904c179d3201ce4
Vic Wen <vic20050418@gmail.com>
2023-10-12 16:44:19 +0800

    end of CPE49

--- CPE/CPE49/main.cpp
+++ CPE/CPE49/main.cpp
@@ -1,22 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void solve() {
-  string s;
-  while (getline(cin, s)) {
-    int tmp;
-    for (auto c : s) {
-      if (isdigit(c))
-        tmp = c - '0';
-      else if (isupper(c))
-        tmp = c - 'A' + 10;
-      else if (islower(c))
-        tmp = c - 'a' + 36;
-      else
-        continue;
-    }
-  }
-}
+void solve() {}
 
 signed main() {
   ios_base::sync_with_stdio(false);
