3cd6f0e70583a17696b1e9d3e3c2da1fdff26b07
Vic Wen <vic20050418@gmail.com>
2023-10-17 16:52:36 +0800

    optimization UVA10931-parity

--- CPE/CPE49/UVA10931-Parity.cpp
+++ CPE/CPE49/UVA10931-Parity.cpp
@@ -1,28 +1,18 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-int main() {
-	ios_base::sync_with_stdio(false); cin.tie(0);
-	long n;
-	while (cin >> n && n) {
-		int ret = 0;
-		string bin = "", tmp = "";
-		while (n >= 2) {
-			if (n & 1) { tmp += "1"; ret++; }
-			else tmp += "0";
+void solution() {
+  long long num;
+  while (cin >> num && num) {
+    bitset<31> b(num);
+    string str = b.to_string();
 
-			n /= 2;
-		}
-		if (n) { tmp += "1"; ret++; }
-		else tmp += "0";
+    int start = 0;
+    while (str[start] == '0') start++;
+    if (start > 0) str.erase(0, start);
 
-		int index = tmp.size() - 1;
-		for (int i = index; i >= 0; i--)
-			if (tmp[i] == '1') { index = i; break; }
-
-		for (int i = index; i >= 0; i--) bin += tmp[i];
-
-		cout << "The parity of " << bin << " is " << ret << " (mod 2).\n";
-	}
-
+    cout << "The parity of " << str << " is " << b.count() << " (mod 2).\n";
+  }
 }
+
+int main() { solution(); }
