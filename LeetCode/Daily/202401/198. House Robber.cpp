227a217647a7e091610a55bf5ceec1e3e5555747
Vic Wen <vic20050418@gmail.com>
2024-01-22 12:54:37 +0800

    202401 Day 21

---
+++ LeetCode/Daily/202401/198. House Robber.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        int n = nums.size();
+        if(n == 1) return nums[0];
+        if(n == 2) return max(nums[0], nums[1]);
+
+        vector<int> dp(n, 0);
+        dp[0] = nums[0];
+        dp[1] = nums[1];
+
+        for(int i=2; i<n; ++i) {
+            dp[i] = max(dp[i-2] + nums[i], dp[i-1]);
+            if(i >= 3) dp[i] = max(dp[i], dp[i-3] + nums[i]);
+        }
+
+        return dp[n-1];
+    }
+};
