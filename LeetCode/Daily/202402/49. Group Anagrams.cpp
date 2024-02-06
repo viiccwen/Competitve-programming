4df4d8591b139a2ae0c83daf6df9130a9169b49a
viiccwen <vic20050418@gmail.com>
2024-02-06 19:58:47 +0800

    202402 Day 6

---
+++ LeetCode/Daily/202402/49. Group Anagrams.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        unordered_map<string, vector<string>> map;
+
+        for (auto str : strs) {
+            string key = str;
+            sort(key.begin(), key.end());
+            map[key].push_back(str);
+        }
+
+        vector<vector<string>> result;
+        for (auto [_, str] : map) {
+            result.push_back(str);
+        }
+
+        return result;
+    }
+};
